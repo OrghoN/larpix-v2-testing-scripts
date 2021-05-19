adfe18c9e4e88e2443c19bd489b852ad8365de87
OrghoN <anoronyo@gmail.com>
2021-05-19 04:53:57 -0500

    cumulative distributions

--- plotting-scripts/pedestal_distributions.py
+++ plotting-scripts/pedestal_distributions.py
@@ -7,12 +7,30 @@ import re
 def distributionPlots(fileList):
     f = open(fileList, "r")
 
+    cumMean = plt.figure(1)
+    cumMeanLog = plt.figure(2)
+    cumStd = plt.figure(3)
+    cumStdLog = plt.figure(4)
+
     for file in f:
         f1 = open (file.strip(), "r")
         out = json.loads(f1.read())
         out = list(map(lambda x: (x[0], np.mean(x[2]), np.std(x[2])), out))
 
         runNo = re.search(r'\d\d\d\d_\d\d_\d\d_\d\d_\d\d_\d\d',file.strip()).group(0)
+
+        plt.figure(1)
+        plt.hist(list(map(lambda x: x[1], out)), label=runNo, bins=200, histtype='step')
+
+        plt.figure(2)
+        plt.hist(list(map(lambda x: x[1], out)), label=runNo, bins=200, histtype='step')
+
+        plt.figure(3)
+        plt.hist(list(map(lambda x: x[2], out)), label=runNo, bins=200, histtype='step')
+
+        plt.figure(4)
+        plt.hist(list(map(lambda x: x[2], out)), label=runNo, bins=200, histtype='step')
+
 
         fig = plt.figure()
         plt.hist(list(map(lambda x: x[1], out)), bins=200, histtype='step')
@@ -51,6 +69,40 @@ def distributionPlots(fileList):
         plt.ylabel('std dev ADC')
         plt.savefig(f"plots/{runNo}_meanStd.png", transparent=True)
         plt.close()
-        
+
+    plt.figure(1)
+    plt.legend(loc='upper right')
+    plt.savefig(f"plots/cum_mean.png", transparent=True)
+    plt.xlabel('mean ADC')
+    plt.ylabel('channel count')
+    plt.close()
+
+    plt.figure(2)
+    plt.legend(loc='upper right')
+    plt.yscale('log', nonposy='clip')
+    plt.xlabel('mean ADC')
+    plt.ylabel('channel count')
+    plt.savefig(f"plots/cum_mean_log.png", transparent=True)
+    plt.close()
+
+    plt.figure(3)
+    plt.legend(loc='upper right')
+    plt.xlabel('std dev ADC')
+    plt.ylabel('channel count')
+    plt.savefig(f"plots/cum_std.png", transparent=True)
+    plt.close()
+
+    plt.figure(4)
+    plt.legend(loc='upper right')
+    plt.yscale('log', nonposy='clip')
+    plt.xlabel('std dev ADC')
+    plt.ylabel('channel count')
+    plt.savefig(f"plots/cum_std_log.png", transparent=True)
+    plt.close()
+
+
+
+
+
 if __name__ == '__main__':
     distributionPlots(sys.argv[1])
